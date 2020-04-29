7ed49e8629c7b5b4b3b95470cc175d6c13e84e2b
Carlos Alberto Hochsprung Júnior <CARLOS.HJUNIOR@lince.com.br>
2020-04-29 14:32:11 -0300

    ...

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -42,23 +42,4 @@ class _MyPageState extends State<MyPage> {
 
 Future<void> taskEmail() async {
   print('executing task');
-  
-  final String username = 'flutter.background@gmail.com';
-  final String password = '@123456789!';
-  print('user $username and pass $password');
-
-//  final SmtpServer smtpServer = gmail(username, password);
-//
-//  final Message message = Message()
-//    ..from = Address(username, 'Background Test')
-//    ..recipients.add('carlos.hjunior@lince.com.br')
-//    ..subject = 'Test Dart Mailer library :: 😀 :: ${DateTime.now()}'
-//    ..text = 'This is the plain text.\nThis is line 2 of the text part.';
-//
-//  try {
-//    await send(message, smtpServer);
-//  } catch (e, _) {
-//    print('erro >> $e');
-//  }
-  print('end task');
 }
